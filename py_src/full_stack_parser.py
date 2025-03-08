39dbeeabedac8093470945d16fee0b0747776802
JYYHH <jin511@purdue.edu>
2025-03-07 19:42:11 -0500

    Fix some bugs (type not found)

--- py_src/full_stack_parser.py
+++ py_src/full_stack_parser.py
@@ -2,52 +2,68 @@ import io
 from pycparser import c_parser, c_ast
 from pcpp import Preprocessor
 
+undefined_type = {
+    'bool',
+    'FILE',
+    'void',
+}
+
 # class for original source code, only to check for the constants
 class MyPreprocessor(Preprocessor):
+    # We override this function, to handle undefined var type (since we do not look into other headers)
+    def expand_macros(self, tokens, expanding_from = []):
+        for i in range(len(tokens)):
+            if tokens[i].value in undefined_type:
+                tokens[i].value = "int" # default type
+        return super().expand_macros(tokens, expanding_from)
+
     # We override this function, to return None for all not found cases
     def on_include_not_found(self, is_system_include, curdir, includepath, io, directive = None):
-        print(f"Warning: Skipping missing include file '{includepath}'")
+        # print(f"Warning: Skipping missing include file '{includepath}'")
         return None  # Returning None tells pcpp to ignore it
 
 # class for preprocessed source code
 class VariableDefinitionChecker(c_ast.NodeVisitor):
-    def __init__(self, grader):
+    def __init__(self, header_len, grader):
         self.errors = []
         self.in_global = True
         self.in_param = False # whether an argument for functions
         self.decl_lines = {}  # line -> list of variable names
         self.grader = grader
+        self.header_len = header_len
 
     # visit the declaration for variables
     def visit_Decl(self, node):
         if node.name is None:
             return
 
-        if not self.in_param and node.coord:
-            line = node.coord.line
-            # save for later Rule XII.A
-            self.decl_lines.setdefault(line, []).append(node.name)
+        # only consider things in .c file
+        if node.coord.line > self.header_len:
+            if not self.in_param and node.coord:
+                line = node.coord.line - self.header_len
+                # save for later Rule XII.A
+                self.decl_lines.setdefault(line, []).append(node.name)
 
-        # Rule I.A: Variable names must be all lowercase.
-        if node.name != node.name.lower():
-            self.errors.append(f"Line {node.coord.line}: Variable '{node.name}' should be all lowercase.")
-            self.grader.update_item("I.A")
+            # Rule I.A: Variable names must be all lowercase.
+            if not node.name.islower():
+                self.errors.append(f"Line {node.coord.line - self.header_len}: Variable '{node.name}' should be all lowercase.")
+                self.grader.update_item("I.A")
 
-        # Rule I.D: Global variables (i.e. at global scope and not function arguments) must start with "g_"
-        if self.in_global and not self.in_param:
-            if not node.name.startswith("g_"):
-                self.errors.append(f"Line {node.coord.line}: Global variable '{node.name}' should start with 'g_'.")
-                self.grader.update_item("I.D")
+            # Rule I.D: Global variables (i.e. at global scope and not function arguments) must start with "g_"
+            if self.in_global and not self.in_param:
+                if not node.name.startswith("g_"):
+                    self.errors.append(f"Line {node.coord.line - self.header_len}: Global variable '{node.name}' should start with 'g_'.")
+                    self.grader.update_item("I.D")
 
-        # Rule XII.B: All variables (non-arguments) must be initialized.
-        if not self.in_param and node.init is None:
-            self.errors.append(f"Line {node.coord.line}: Variable '{node.name}' must be initialized at the time it is defined.")
-            self.grader.update_item("XII.B")
+            # Rule XII.B: All variables (non-arguments) must be initialized.
+            if not self.in_param and node.init is None:
+                self.errors.append(f"Line {node.coord.line - self.header_len}: Variable '{node.name}' must be initialized at the time it is defined.")
+                self.grader.update_item("XII.B")
 
-        # Rule XII.D: Variable length arrays are prohibited.
-        if self._is_vla(node.type):
-            self.errors.append(f"Line {node.coord.line}: Variable length arrays are prohibited.")
-            self.grader.update_item("XII.D")
+            # Rule XII.D: Variable length arrays are prohibited.
+            if self._is_vla(node.type):
+                self.errors.append(f"Line {node.coord.line - self.header_len}: Variable length arrays are prohibited.")
+                self.grader.update_item("XII.D")
 
         # Recursion check for children on AST
         self.generic_visit(node)
@@ -93,6 +109,7 @@ def whole_check(src_address: str, grader):
     # read in the original source code
     header_file = open(src_address + '.h', "r")
     code_original = header_file.read()
+    header_lines_number = len(code_original.split('\n'))
     code_original += '\n'
     header_file.close()
     src_file = open(src_address + '.c', "r")
@@ -123,7 +140,7 @@ def whole_check(src_address: str, grader):
         print("Parsing error:", e)
         return
 
-    checker = VariableDefinitionChecker(grader)
+    checker = VariableDefinitionChecker(header_lines_number, grader)
     checker.visit(ast)
     checker.finalize()
 
